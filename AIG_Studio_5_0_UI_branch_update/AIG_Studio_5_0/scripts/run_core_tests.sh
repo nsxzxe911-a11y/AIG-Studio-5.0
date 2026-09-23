004ceaa625d741d765cff4241153908f0a1c0939
FanucCAM <nsxzxe911@gmail.com>
2026-09-23 00:54:06 +0000

    Include AI tolerance inspection in core regression build

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/scripts/run_core_tests.sh
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/scripts/run_core_tests.sh
@@ -7,6 +7,7 @@ kotlinc \
   "$ROOT/core/src/main/kotlin/com/aigstudio/core/Document.kt" \
   "$ROOT/core/src/main/kotlin/com/aigstudio/core/Transform.kt" \
   "$ROOT/core/src/main/kotlin/com/aigstudio/core/Cam.kt" \
+  "$ROOT/core/src/main/kotlin/com/aigstudio/core/AiAssist.kt" \
   "$ROOT/core/src/test/kotlin/com/aigstudio/core/CoreRegressionTest.kt" \
   -include-runtime -d "$ROOT/build/core-tests.jar"
 java -jar "$ROOT/build/core-tests.jar"
