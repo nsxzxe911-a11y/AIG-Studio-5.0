f4b601000d69961d5b866dda741e343ccdcb0806
FanucCAM <nsxzxe911@gmail.com>
2026-09-24 12:33:36 +0000

    bump AIG Studio to 5.22.0 CNC keypad upgrade

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aigstudio.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 52100
-        versionName = "5.21.0"
+        versionCode = 52200
+        versionName = "5.22.0"
     }
 }
 
