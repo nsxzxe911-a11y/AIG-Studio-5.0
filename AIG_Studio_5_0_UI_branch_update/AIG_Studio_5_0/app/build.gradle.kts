36ef474e02b723f7c3f076cbea69ee16dce153b6
FanucCAM <nsxzxe911@gmail.com>
2026-09-24 12:29:11 +0000

    bump AIG Studio to 5.21.0 stock workflow upgrade

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aigstudio.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 52000
-        versionName = "5.20.0"
+        versionCode = 52100
+        versionName = "5.21.0"
     }
 }
 
