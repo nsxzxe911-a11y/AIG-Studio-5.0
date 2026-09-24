53a08f781f7b8b6a58b2e8efcfefa8320c8406ad
FanucCAM <nsxzxe911@gmail.com>
2026-09-24 09:23:30 +0000

    Compile environment settings in core regression

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/scripts/run_core_tests.sh
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/scripts/run_core_tests.sh
@@ -10,6 +10,7 @@ kotlinc \
   "$ROOT/core/src/main/kotlin/com/aigstudio/core/Machining3D.kt" \
   "$ROOT/core/src/main/kotlin/com/aigstudio/core/AiAssist.kt" \
   "$ROOT/core/src/main/kotlin/com/aigstudio/core/PackageBundle.kt" \
+  "$ROOT/core/src/main/kotlin/com/aigstudio/core/EnvironmentSettings.kt" \
   "$ROOT/core/src/test/kotlin/com/aigstudio/core/CoreRegressionTest.kt" \
   -include-runtime -d "$ROOT/build/core-tests.jar"
 java -jar "$ROOT/build/core-tests.jar"
