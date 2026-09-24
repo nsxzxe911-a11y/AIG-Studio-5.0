491ee693b103fc69c379460506d3f9459917dddc
FanucCAM <nsxzxe911@gmail.com>
2026-09-24 13:55:09 +0000

    promote AIG Studio to 7.0.0 milestone

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aigstudio.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 60000
-        versionName = "6.0.0"
+        versionCode = 70000
+        versionName = "7.0.0"
     }
 }
 
