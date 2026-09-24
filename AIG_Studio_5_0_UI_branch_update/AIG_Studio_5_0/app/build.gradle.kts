4baa74f98d770f5c638eeab462af8cb4a4c5f81d
FanucCAM <nsxzxe911@gmail.com>
2026-09-24 12:11:05 +0000

    bump AIG Studio to 5.17.0 native arc upgrade

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aigstudio.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 51600
-        versionName = "5.16.0"
+        versionCode = 51700
+        versionName = "5.17.0"
     }
 }
 
