5fd2f3160f9312ed5833badda13966f3c39ce91d
FanucCAM <nsxzxe911@gmail.com>
2026-09-24 13:39:34 +0000

    upgrade AIG Studio to 5.33.0

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aigstudio.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 53200
-        versionName = "5.32.0"
+        versionCode = 53300
+        versionName = "5.33.0"
     }
 }
 
