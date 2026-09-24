03d6f919e4455ea862066d24db00b009384a5726
FanucCAM <nsxzxe911@gmail.com>
2026-09-24 11:43:46 +0000

    bump AIG Studio to 5.7.0 CAD upgrade

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aigstudio.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 50600
-        versionName = "5.6.0"
+        versionCode = 50700
+        versionName = "5.7.0"
     }
 }
 
