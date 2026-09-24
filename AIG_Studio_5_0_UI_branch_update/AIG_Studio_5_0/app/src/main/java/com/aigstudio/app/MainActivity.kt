679a6b9556cfaedef49a71270085d049552f392c
FanucCAM <nsxzxe911@gmail.com>
2026-09-24 09:25:54 +0000

    Import environment settings Android controls

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/src/main/java/com/aigstudio/app/MainActivity.kt
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/src/main/java/com/aigstudio/app/MainActivity.kt
@@ -18,6 +18,9 @@ import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Spinner
+import android.widget.SeekBar
+import android.widget.ArrayAdapter
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.LinearLayout
