9e7c25b52b103113a8c2d9ccba5cad7565cbf04f
FanucCAM <nsxzxe911@gmail.com>
2026-09-24 11:51:21 +0000

    bump AIG Studio to 5.10.0 tangent snap upgrade

--- AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
+++ AIG_Studio_5_0_UI_branch_update/AIG_Studio_5_0/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aigstudio.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 50900
-        versionName = "5.9.0"
+        versionCode = 51000
+        versionName = "5.10.0"
     }
 }
 
